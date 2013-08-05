e56de187fcf36cdac6c3cb4fb4e72099a5a6be13
jichuntao <jichuntao@hotmail.com>
2013-08-06 02:01:38 +0800

    hi ibqq

---
+++ test.js
@@ -0,0 +1,41 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jichuntao
+ * Date: 13-8-3
+ * Time: 下午11:53
+ * To change this template use File | Settings | File Templates.
+ */
+
+var fs = require('fs');
+var IBQQ = require('./lib/IBQQ');
+
+var ibqq = IBQQ.create();
+var qqnum = '1234';
+var qqpw = 'xxx';
+
+ibqq.on('group_message', function (value) {
+    var from_uin = value.from_uin;
+    if (value.content.length == 2 && typeof(value.content[1]) == 'string') {
+        ibqq.command('channel', 'sendqunmsg', [from_uin, value.content[1]], function (err, chk) {
+            console.log(chk);
+        });
+    }
+});
+
+ibqq.on('log', function (data) {
+    console.log(data);
+});
+
+ibqq.on('err', function (data) {
+    console.log(data);
+});
+
+ibqq.on('vcode', function (vr) {
+    fs.watchFile('./vcode', function (curr, prev) {
+        var strs = fs.readFileSync('./vcode', 'utf8');
+        fs.unwatchFile('./vcode');
+        ibqq.inputVcode(strs);
+    });
+});
+
+ibqq.start(qqnum, qqpw);
